93c110ea42d10db12548d6b85ddcb5c5bf4f76ee
Mickey Chiu <chiu@bnl.gov>
2018-03-31 14:36:06 -0500

    Modified to use up to 17 channels

--- DT5742/dt5742.C
+++ DT5742/dt5742.C
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+#include <stdio.h>
 #include <TFile.h>
 #include <TTree.h>
 #include <TH1.h>
@@ -12,199 +13,61 @@
 #include <TF1.h>
 #include <TString.h>
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
-
 #include "dt5742.h"
 
-// should put these in a separate library macro
-
-// return pedestal
-// (should remove outliers)
-double getpedestal(TGraphErrors *g)
-{
-  // go up to t=20 ns to determine pedestal
-  //const int nsamp = g->GetHistogram()->FindBin( 20. );
-  //const int last_samp = 50;
-  const int last_samp = 20;
-  double nsamp = 0.;
-  double sum = 0.;
-  for (int isamp=10; isamp<last_samp; isamp++)
-    {
-      Double_t t,v;
-      g->GetPoint(isamp,t,v);
-      //cout << isamp << "\t" << t << "\t" << v << endl;
-      sum += v;
-      nsamp += 1.0;
-    }
-  sum /= nsamp;
-
-  //cout << "ped " << sum << endl;
-  return sum;
-}
-
-
-// return amplitude, simple max (or min if negative)
-// assumes negative, should modify so that amp gives sign
-void getamplitude(TGraph *g, double& amp, int& maxsamp)
-{
-  //cout << "in getamplitude()" << endl;
-  maxsamp = -1;
-  amp = 9999.;
-  // skip first 10 and last 20 since they are usually bad
-  for (int isamp=10; isamp<1000; isamp++)
-    {
-      Double_t t,v;
-      g->GetPoint(isamp,t,v);
-      if ( v < amp )
-        {
-          amp = v;
-          maxsamp = isamp;
-        }
-    }
-
-  //cout << amp << "\t" << maxsamp << endl;
-}
-
-// return amplitude, simple max (or min if negative)
-double getintegral(TGraph *g, double min, double max)
-{
-  int minsamp = min/0.2; // quick and dirty way to get sample
-  int maxsamp = max/0.2;
-
-  double sum = 0.;
-
-  // now do integral \int{Vdt}
-  Double_t t0,v0;
-  Double_t t1,v1;
-  for (int isamp=minsamp; isamp<maxsamp; isamp++)
-    {
-      g->GetPoint(isamp,t0,v0);
-      g->GetPoint(isamp+1,t1,v1);
-
-      double v = (v0+v1)/2.0;
-      sum += v*(t1-t0);
-    }
-
-  return sum;
-}
-
-// Find time at midpoint
-double gettime(TGraph *g, double amp, int maxsamp)
-{
-  double start_time = -9999;
-
-  // check sign
-  /*
-  double sign = 1;
-  if ( amp<0 ) sign = -1;
-  */
-
-  for (int isamp=maxsamp; isamp>=0; isamp--)
-  {
-    Double_t t1,v1;
-    g->GetPoint(isamp,t1,v1);
-    if ( v1 > (amp/2.0) )
-    {
-      Double_t t2,v2;
-      g->GetPoint(isamp+1,t2,v2);
-      // interpolate time
-      start_time = (amp/2.0-v1)*(t2-t1)/(v2-v1)+t1;
-      //cout << "nch " << nch << "\t" << start_time[nch] << endl;
-      break;
-    }
-  }
-
-  return start_time;
-}
-
-// Find time at (neg) threshold between tmin and tmax (tmin,tmax are in ns)
-double gettime(TGraph *g, double thresh, double tmin, double tmax)
-{
-  double start_time = -9999.;
-  // quick and dirty way to get sample number, since each sample is ~200 ps
-  int begin_samp = static_cast<int>(tmin/0.2);
-  int end_samp = static_cast<int>(tmax/0.2);
-
-  for (int isamp=begin_samp; isamp<end_samp; isamp++)
-    {
-      Double_t t1,v1;
-      g->GetPoint(isamp,t1,v1);
-      if ( v1 < thresh )
-        {
-          Double_t t0,v0;
-          g->GetPoint(isamp-1,t0,v0);
-          // interpolate time
-          double slope = (v1-v0)/(t1-t0);
-          if ( slope!= 0 )
-          {
-            start_time = t0 + (thresh - v0)/slope;
-          }
-          else
-          {
-            cout << "ERROR in gettime(), v1==v0" << endl;
-            return -2.0;
-          }
-
-          //cout << "stime\t" << start_time << endl;
-          break;
-        }
-    }
-
-  return start_time;
-}
-
-
-// Find time at (neg) threshold between tmin and tmax (tmin,tmax are in ns)
-double gettimeCFD(TGraph *g, double fraction, int delay, double tmin, double tmax)
-{
-  double start_time = -9999.;
-  // quick and dirty way to get sample number, since each sample is ~200 ps
-  int begin_samp = static_cast<int>(tmin/0.2);
-  int end_samp = static_cast<int>(tmax/0.2);
-
-  // Generate v'_i = fraction*v_i - v_(i-delay)
-  Int_t n = 0;
-  Double_t t[1024], v[1024];
-  Double_t t1, v1;
-  Double_t t2, v2;
-  for (int isamp=begin_samp; isamp<end_samp; isamp++)
-  {
-    if ( isamp+delay>1023 ) break;
-    g->GetPoint(isamp,t1,v1);
-    g->GetPoint(isamp+delay,t2,v2);
-
-    t[n] = t2;
-    v[n] = fraction*v2 - v1;
-    n++;
-  }
-
-  TGraph g_cfd(n,t,v);
-  g_cfd.Draw("ap");
-  cout << "in gettimeCFD()" << endl;
-  string junk;
-  cin >> junk;
-
-  return start_time;
-}
-
-// Open each wave_NN.txt file
+// Open each wave_NN.txt or .dat file
+// binorasc: 0=text, 1=binary
 // Each wave file corresponds to 1 channel
-int OpenWaveFiles()
+int OpenWaveFiles(const int binorasc)
 {
   TString name;
-  for (int ich=0; ich<MAXCH; ich++)
+  int maxch = MAXCH;
+
+  if ( MAXCH==17 ) // digitize TR0 channel
+  {
+    maxch = 16;
+    // open up wave files
+    // (should maybe include TR_x_x.txt/dat files)
+    if ( binorasc==0 )
+    {
+      name = "TR_0_0.txt";
+      wavefile[maxch].open( name.Data() );
+    }
+    else
+    {
+      name = "TR_0_0.dat";
+      wavefile_fp[maxch] = fopen( name.Data(), "rb" );
+    }
+
+    // make trace graphs of each channel in the event
+    trace[maxch] = new TGraphErrors();
+    name = "trace"; name += maxch;
+    trace[maxch]->SetName( name );
+    trace[maxch]->SetMarkerColor( maxch%8+1 );
+    trace[maxch]->SetLineColor( maxch%8+1 );
+  }
+
+  for (int ich=0; ich<maxch; ich++)
   {
     // open up wave files
-    name = "wave_"; name += ich; name += ".txt";
-    wavefile[ich].open( name.Data() );
+    // (should maybe include TR_x_x.txt/dat files)
+    if ( binorasc==0 )
+    {
+      name = "wave_"; name += ich; name += ".txt";
+      wavefile[ich].open( name.Data() );
+    }
+    else
+    {
+      name = "wave_"; name += ich; name += ".dat";
+      wavefile_fp[ich] = fopen( name.Data(), "rb" );
+    }
 
     // make trace graphs of each channel in the event
     trace[ich] = new TGraphErrors();
     name = "trace"; name += ich;
     trace[ich]->SetName( name );
     trace[ich]->SetMarkerColor( ich%8+1 );
+    trace[ich]->SetLineColor( ich%8+1 );
     //if ( ich==9 ) trace[ich]->SetMarkerColor( 2 );
     trace[ich]->SetMarkerStyle( 20 );
     trace[ich]->SetMarkerSize( 0.5 );
@@ -217,17 +80,24 @@ int OpenWaveFiles()
   return 1;
 }
 
-int CloseWaveFiles()
+int CloseWaveFiles(const int binorasc)
 {
   for (int ich=0; ich<MAXCH; ich++)
+  {
+    if ( binorasc == 0 )
     {
       wavefile[ich].close();
     }
+    else
+    {
+      fclose( wavefile_fp[ich] );
+    }
+  }
 
   return 1;
 }
 
-int ReadSingleEvent()
+int ReadSingleEventAscii()
 {
   string FullLine;
   string junk1, junk2, junk3;
@@ -324,7 +194,7 @@ int ReadSingleEvent()
           line.clear();
           line.str( FullLine.c_str() );
           line >> adc[ich][0];
-          cout << adc[ich][0];
+          //cout << adc[ich][0];
           start_samp = 1;
         }
 
@@ -354,6 +224,106 @@ int ReadSingleEvent()
             }
         }
     }
+
+  return 1;
+}
+
+
+int ReadSingleEventBinary()
+{
+//  string FullLine;
+//  string junk1, junk2, junk3;
+//  istringstream line;
+  const int verbose = 0;
+  
+  g_record_len = 0;  // number of samples
+  g_board_id = 0;    // board id
+  g_channel = 0;     // channel
+  g_event_num = -1;  // event number (starting from 0!)
+  g_pattern = -1;    // pattern?
+  g_time_stamp = 0; // time stamp?
+  g_dc_offset = -1;  // DC offset
+  g_start_cell = -1; // starting cell of readout
+
+  for (int ich=0; ich<MAXCH; ich++)
+  {
+    max_ampl[ich] = 0;
+    min_ampl[ich] = 5000;
+    min_samp[ich] = -999;
+    max_samp[ich] = -999;
+    min_time[ich] = -999.;
+    max_time[ich] = -999.;
+
+    // get one line first 
+    size_t nread = fread(&g_record_len,sizeof(uint32_t),1,wavefile_fp[ich]);
+    if ( nread != 1 )
+    {
+      return 0;
+    }
+    fread(&g_board_id,sizeof(uint32_t),1,wavefile_fp[ich]);
+    fread(&g_pattern,sizeof(uint32_t),1,wavefile_fp[ich]);
+    fread(&g_channel,sizeof(uint32_t),1,wavefile_fp[ich]);
+    fread(&g_event_num,sizeof(uint32_t),1,wavefile_fp[ich]);
+    fread(&g_time_stamp,sizeof(uint32_t),1,wavefile_fp[ich]);
+
+    if ( verbose>0 )
+    {
+      cout << "*** CH "  << ich << endl;
+      cout << "len\t"    << g_record_len << endl;
+      cout << "board\t"  << g_board_id << endl;
+      cout << "patt\t"   << g_pattern << endl;
+      cout << "ch\t"     << g_channel << endl;
+      cout << "evt\t"    << g_event_num << endl;
+      cout << "tstamp\t" << g_time_stamp << endl;
+    }
+
+    /*
+    if ( g_channel != ich%8 )
+    {
+      cout << "ERROR, " << g_channel << " != " << ich << endl;
+    }
+    */
+
+    /*
+    if ( ich==16 )
+    {
+      cout << "16 16 16" << endl;
+    }
+    */
+    //trace[ich]->Reset();
+    for (int isamp=0; isamp<1024; isamp++)
+    {
+      size_t nread = fread(&adc[ich][isamp],sizeof(float),1,wavefile_fp[ich]);
+      if ( nread != 1 )
+      {
+        return 0;
+      }
+
+      float time = isamp*DATA_TIMESTEP; // DATA_TIMESTEP is in ns
+      trace[ich]->SetPoint(isamp,time,adc[ich][isamp]);
+      trace[ich]->SetPointError(isamp,0.,1.0);  // should change the error
+
+/*
+if ( ich==16 )
+{
+cout << isamp << "\t" << time << "\t" << adc[ich][isamp] << endl;
+}
+*/
+
+      if ( max_ampl[ich] < adc[ich][isamp] )
+      {
+        max_ampl[ich] = adc[ich][isamp];
+        max_samp[ich] = isamp;
+        max_time[ich] = time;
+      }
+      if ( min_ampl[ich] > adc[ich][isamp] )
+      {
+        min_ampl[ich] = adc[ich][isamp];
+        min_samp[ich] = isamp;
+        min_time[ich] = time;
+      }
+    }
+  }
 
   return 1;
 }
--- DT5742/dt5742.h
+++ DT5742/dt5742.h
@@ -3,13 +3,12 @@
 
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
 
 
-
-//const int MAXCH = 16;
-const int MAXCH = 1;
+const int MAXCH = 17;
 //const int MAXCH = 14;
 //const int MAXCH = 6;
 const int NSAMPLES = 1024;
@@ -24,17 +23,20 @@ float max_time[MAXCH];        // time of max amplitude
 float min_time[MAXCH];        // time of min amplitude
 
 ifstream wavefile[MAXCH];
-int g_record_len;  // number of samples
-int g_board_id;    // board id
-int g_channel;     // channel
-int g_event_num;   // event number (starting from 0!)
-int g_pattern;     // pattern?
-UInt_t g_time_stamp;  // time stamp?
+FILE* wavefile_fp[MAXCH];
+uint32_t g_record_len;  // number of samples
+uint32_t g_board_id;    // board id
+uint32_t g_channel;     // channel
+uint32_t g_event_num;   // event number (starting from 0!)
+uint32_t g_pattern;     // pattern?
+uint32_t g_time_stamp;  // time stamp?
 int g_dc_offset;   // DC offset
 int g_start_cell;  // starting cell of readout
 
 // text
-const float DATA_TIMESTEP = 0.2;  // in nanoseconds
+//const float DATA_TIMESTEP = 0.2;  // in nanoseconds
+const float DATA_TIMESTEP = 0.5;  // in nanoseconds
+//const float DATA_TIMESTEP = 1.0;  // in nanoseconds
 const int   DIGI_STEPS = 4000;      // number of steps in template
 const float DIGI_START_TIME = -20.;
 const float DIGI_END_TIME = 20.;
@@ -42,9 +44,8 @@ const float DIGI_TIMESTEP = (DIGI_END_TIME - DIGI_START_TIME)/DIGI_STEPS;
 TGraph *digi_graph[MAXCH];
 float digi_pulse[MAXCH][DIGI_STEPS] = {{0.}};
 
-
 Double_t FitFcn(Double_t *x, Double_t *par);
 void set_fitpulse(const int ich);
-//double getpedestal(TGraphErrors *g);
 
 #endif  // __PSTOF_DT5742__
+
